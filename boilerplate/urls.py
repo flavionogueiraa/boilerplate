07d91684c42a3579bb0fe3606a492bf7ddfdd39b
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2023-10-03 21:36:01 -0300

    rest framework urls

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     # Libs urls
+    path("api-auth/", include("rest_framework.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
