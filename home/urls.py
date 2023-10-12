0fc1c225ac2dfb798e041d427e3cc869e04dd08f
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2023-10-12 12:00:48 -0300

    Separating home

--- home/urls.py
+++ home/urls.py
@@ -3,14 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("elements/", views.elements, name="elements"),
-    path("generic/", views.generic, name="generic"),
-    path("shannon-fano/", views.shannon_fano_view, name="shannon_fano_view"),
-    path("huffman/", views.huffman_view, name="huffman_view"),
-    path(
-        "meme-generator/",
-        views.meme_generator_view,
-        name="meme_generator_view",
-    ),
     path("", views.home, name="home"),
 ]
